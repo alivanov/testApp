27251e5e731a88caa3ca43dfa40767354ba6bedf
Alexey Ivanov <ivanov@intspirit.com>
2016-01-20 15:18:28 +0300

    page SURVEYS

--- assets/js/controllers/SurveyCollectionController.js
+++ assets/js/controllers/SurveyCollectionController.js
@@ -1,4 +1,27 @@
 angular.module('starter.controllers').controller('SurveyCollectionController',
-  function($scope) {
+  function($scope,
+           API,
+           $ionicPopup) {
+
+    var init = function() {
+      API.Collections.getCollections()
+        .success(function(res) {
+          $scope.surveys = res.pages.surveycollection;
+          $scope.item = _.findWhere($scope.surveys.items[0].questions, {id: '6'});
+        })
+        .error(function(err) {
+          console.error(err);
+        });
+    };
+
+    $scope.submit = function(data) {
+      $ionicPopup.alert({
+        title: 'Submitted!',
+        template: 'Submitted option: ' + data.text
+      });
+      console.log('submitted', data);
+    };
+
+    init();
 
   });
